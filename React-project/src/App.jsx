0fda579d0ed7a4ad81a2fb51f5021d931dfe4ed1
krittaphong <krittaphong3012@gmail.com>
2024-02-27 10:14:30 +0700

    Second-Done

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -1,12 +1,24 @@
+import Contact from "./assets/Contact"
+import Hello from "./assets/Hello"
+import Counter from "./assets/Counter";
+
 function App() {
 
+  const helloData = [
+    {name: "Mike", surname: "Brow"},
+    {name: "Bob", surname: "Swagger"}
+  ];
+
   return (
-      <div>
-        <h1>Hello</h1>
-        <h2>My First React App.</h2>
-      </div>
-      
-  )
+    <div className='App'>
+        < Counter />
+        {helloData.map((data, index) => (
+          <Hello key={index} name={data.name} message={data.message}/>
+        ))}
+
+        <Contact email="krittaphong2546@gmail.com" phone ="0985535297"/>
+    </div>
+  );
 }
 
 export default App
